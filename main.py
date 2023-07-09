b55cf36408f6b0564165dd83a2edd4497da7a862
Pinpunyu <pe25617259@gmail.com>
2023-07-09 15:32:28 +0800

    agent observation

--- main.py
+++ main.py
@@ -1,28 +1,49 @@
 from utils.agent import Agent
+from utils.environment import nuk_town_init
 from anytree import Node, RenderTree
 
-Nuk = Node("Nuk")
-School = Node("School", parent=Nuk)
-classrooom1 = Node("classrooom1", parent=School)
-classrooom2 = Node("classrooom2", parent=School)
-Home = Node("Home", parent=Nuk)
-bedrooom = Node("bedrooom", parent=Home)
-bathrooom = Node("bathrooom", parent=Home)
-Park = Node("Park", parent=Nuk)
-Store = Node("Store", parent=Nuk)
+# Nuk = Node("Nuk")
+# School = Node("School", parent=Nuk)
+# classrooom1 = Node("classrooom1", parent=School)
+# classrooom2 = Node("classrooom2", parent=School)
+# Home = Node("Home", parent=Nuk)
+# bedrooom = Node("bedrooom", parent=Home)
+# bathrooom = Node("bathrooom", parent=Home)
+# Park = Node("Park", parent=Nuk)
+# Store = Node("Store", parent=Nuk)
 
-# print(udo)
-# Node('/Udo')
-# print(joe)
-# Node('/Udo/Dan/Joe')
 
-for pre, fill, node in RenderTree(Nuk):
-    print("%s%s" % (pre, node.name))
-print("")
+# for pre, fill, node in RenderTree(Nuk):
+#     print("%s%s" % (pre, node.name))
+# print("")
 
-pinyu = Agent("Pinyu", "21 years old is a student", Nuk)
+{
+    "name" : "nuk" ,
+    "agent" : [],
+    "state" : None
+}
 
-known = ["Home", "School", "Park"]
 
-pinyu.create_my_tree(known)
-pinyu.print_self_knowledge()
+nuk_town = nuk_town_init()
+print(nuk_town.observation("NUK Town:Pinyu's home", 0))
+# nuk_town.insert_root("123456")
+# print(nuk_town)
+# nuk_town.visualize()
+
+
+
+pinyu = Agent("Pinyu", "21 years old is a student")
+
+
+
+
+# pinyu.print_self_knowledge()
+
+# known = ["Pinyu's home", "school"]
+# pinyu.agent_tree_init(known)
+
+# pinyu.new_observation(nuk_town, "NUK Town:Pinyu's home")
+# pinyu.new_observation(nuk_town, "NUK Town:Pinyu's home:room")
+# pinyu.new_observation(nuk_town, "NUK Town")
+# pinyu.new_observation(nuk_town, "NUK Town:Yui's home")
+# print(pinyu.get_memory(5))
--- utils/agent.py
+++ utils/agent.py
@@ -1,47 +1,85 @@
-from anytree import Node, RenderTree
+from utils.environment import env_tree
+from utils.environment import tree_node
+from anytree import Node, RenderTree, AsciiStyle
 
 
 class Agent:
     
-    def __init__(self, name, self_knowledge, map, memory = []):
+    def __init__(self, name, self_knowledge, memory_stream = []):
         self.name = name
         self.self_knowledge = self_knowledge
-        self.map = map
-        self.memory = memory
+        # self.map = map
+        self.memory_stream = memory_stream
+        self.tree = env_tree()
+        self.location = ""
+        self.current_action = "sleep"
+        self.current_conversation = None
     
-    def __insert_child(self, parent):
+    def __insert_child(self, parent, current):
+        
+        node = parent+":"+current
+        childs = self.map.__iter_around_env__(node, 0) 
+        
+        for child in childs:
+            child_name = child.split(':')[-1]
+            parent_name = ':'.join(child.split(':')[:-1])
+            
+            if child_name != "" and ":" not in child_name:
+                self.tree.add_children_node(node , child_name)
+                self.__insert_child(node, child_name)
 
-        childs = 0 # map.get_clild(self.map)
-        if childs == 0:
-            return
-        else:
-            for child in childs:
-                second = Node(child, parent=parent)
-                self.__insert_child(second)
-
-    def create_my_tree(self, known):
+    def agent_tree_init(self, known):
 
         # 建立自己的root
-        root = Node(self.name)
+        self.tree = env_tree("NUK Town")
 
         for place in known:
-            first = Node(place, parent=root)
-            self.__insert_child(first)
+            self.tree.add_children_node("NUK Town" , place)
             # 繼續新增子節點
-        
-        for pre, fill, node in RenderTree(root):
-            print("%s%s" % (pre, node.name))
+            self.__insert_child(self.map.root, place)
+            
+        self.tree.visualize()
 
     def print_self_knowledge(self):
         print(f"My name is {self.name}. {self.self_knowledge}")
+        
 
-    def new_observation(self, place):
+    def new_observation(self, map, place):
+
         # 要是place沒有在自己的tree裡，要新增
-
+        # if findall(self.root, filter_=lambda node: node.name in (place)) == 0:
+        #     first = Node(place, parent=self.root)
+        #     self.__insert_child(first)
         # 將place的observation push進memory stream
-        self.memory.push("")# self.memory.push(get_place_observation(place))
+
+        # print(f"{self.location}-{self.location.count(':')} {place}-{place.count(':')}")
+        print(place)
+        child_name = place.split(':')[-1]
+        parent_name = ':'.join(place.split(':')[:-1])
+
+        if self.tree == None:
+            print("begin")
+            self.tree = env_tree(place)
+        elif place.count(":")==0:
+            print("hi")
+            self.tree.insert_root(place)
+        else:
+            print("byt")
+            self.tree.add_children_node(parent_name, child_name)
+            # self.tree.insert_root(place)
+            # self.tree.visualize()
+
+        # self.tree.visualize()
+        self.location = place
+
+        
+
+
+        observations = map.observation(place)
+        print(observations)
+        # for observation in observations:
+        #     self.memory_stream.append(observation)
         
     def get_memory(self, num):
 
-        for i in num:
-            self.memory.pop()
+        return self.memory_stream[-num:]
