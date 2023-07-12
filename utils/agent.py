90c25ccfef78bd53e61f770d8db3a697a08272c8
Yui-Arthur <arthur80633@gmail.com>
2023-07-12 23:04:23 +0800

    agent add a new tick process & update react / dialogue process

--- utils/agent.py
+++ utils/agent.py
@@ -13,13 +13,60 @@ class Agent:
         # self.tree = env_tree()
         self.current_action = None
         self.current_conversation = None
+        self.current_plan_idx = 0
+
         self.memory_stream = []
         self.conversation_history : list[dict] = []
         self.load_json(json)
         self.knows_tree = env_tree(places = [self.location])
 
-        
+    def start_of_day(self , time):
+        ### new day init ###
+        self.current_plan_idx = 0
+        self.current_action = None
+        self.current_conversation = None
+        self.actions[str(time.date())] = []
+        self.__gen_plan__(time)
     
+    def next_tick(self , observations : list[str] , places:list[str] , time:datetime , reflect_threshold : float):
+
+        # 1. get observations & update knows places
+        current_memory_stream , avg_importantance = self.update_observation(observations , time)
+        self.update_knows_places(places)
+
+        # 2. reflection
+        if avg_importantance > reflect_threshold:
+            self.__gen_reflection__(time , 50)
+
+        # 3. current plan
+        plan_time = self.plans[str(time.date())]['detail_plan'][self.current_plan_idx]['time']
+        plan_time = datetime.datetime.strptime(plan_time , '%H:%M')
+        
+        while time.time() > plan_time.time():
+            self.current_plan_idx += 1
+
+        self.current_action = self.plans[str(time.date())]['detail_plan'][self.current_plan_idx]['plan']
+
+
+        # 4. dialogue
+        if self.current_conversation != None and self.current_conversation['target'] != self.name:
+            self.__gen_dialogue__()
+        else:
+        # 5. react
+            for observation in current_memory_stream:
+                is_react = self.__gen_reaction__(observation , time)
+                if is_react: break
+
+        # 6. move
+        self.__agent_move__(places)
+
+        # 7. log actions
+        self.actions[str(time.date())].append({
+            "time" : str(time.time()) ,
+            "action" : self.current_action
+        })
+
+
     def load_json(self , json_file : Union[str , Path]):
         with open(json_file) as f:
             agent_info = json.load(f)
@@ -31,21 +78,27 @@ class Agent:
         self.currently = agent_info['Currently']
         self.lifestyle = agent_info['Lifestyle']
         self.location = agent_info['location']
-        self.plans = agent_info['Plan']
+        self.plans : dict[str , dict[str , list]] = agent_info['Plan']
         
         self.actions = agent_info['action']
 
-    def update_observation(self , observations : list[str] , time : datetime):
+    def update_observation(self , observations : list[dict] , time : datetime) -> Tuple[list[dict] , float]:
 
         importantance = Retrieval.get_importantance(observations)
+        current_memory_stream = []
+
         for idx , observation in enumerate(observations):
-            self.memory_stream.append({
-                "observation" : observation,
-                "observed_entity" : observation.split(" ")[0],
+            memory = {
+                "observation" : observation['observation'],
+                "observed_entity" : observation['observation'].split(" ")[0],
                 "time" : time,
                 "last_used" : datetime.datetime(2023,1,1,0,0,0),
-                "importantance" : importantance[idx]
-            })
+                "importantance" : importantance[idx],
+                "entity_type" : observation['type']
+            }
+            self.memory_stream.append(memory)
+
+        return current_memory_stream , importantance.sum() / len(observations)
 
     def update_knows_places(self , places : list[str]):
         self.knows_tree.add_places(places)
@@ -60,32 +113,61 @@ class Agent:
 
         return ret            
     
-    def __gen_plan__(self , date : datetime):
+    def __gen_plan__(self , time : datetime):
         ### gen the agent's plan ###
-        ### TODO recursive / summarize prev day ###
+        ### TODO recursive / agent recent experiences ###
 
         # summary = self.__gen_summary__(str(date - datetime.timedelta(days=1)))
         prev_plan = ""
 
-        prev_day = (date - datetime.timedelta(days=1)).date()
+        prev_day = (time - datetime.timedelta(days=1)).date()
 
 
-        for idx , plan in enumerate(self.plans[str(prev_day)]):
+        for idx , plan in enumerate(self.plans[str(prev_day)]["rough_plan"]):
             prev_plan += f"{idx+1}) {plan}, "
 
         prompt = (
             f"Name: {self.name} (age: {self.age})\n"
             f"Innate traits: {self.innate_tendency}\n"
-            ### !!! TODO summarize prev day !!! ###
+            ### !!! TODO agent recent experiences !!! ###
             f",{prev_plan}\n"
-            f"Today is {date}. Here is {self.name}’s plan today in broad strokes: 1)\n"
+            f"Today is {time}. Here is {self.name}'s plan today in broad strokes: 1)\n"
             )
         
         ### !!! TODO LLM output !!! ###
-        plans = "1)sleep , 2) go to school"
+        plans = "1)sleep at 8:00 am , 2) go to school at 8:30 am"
 
 
-        self.plans[str(date)] = [i.split(')')[-1] for i in plans.split(",")]
+        self.plans[str(time.date())] = {
+            'rough_plan' : [i.split(')')[-1] for i in plans.split(",")],
+            'detail_plan' : []
+        }
+        
+        self.__gen_detail_plan__(time)
+
+    def __gen_detail_plan__(self , time):
+        ### gen detail plan with finer-grained ###
+        ### TODO prompt / recursize ###
+
+
+        ### !!! TODO LLM output !!! ###
+        plan = (
+            "4:00 am: grab a light snack, such as a piece of fruit, a granola bar, or some nuts.\n"
+            "4:50 pm: take a few minutes to clean up his workspace."
+        )
+
+        # plan string operation
+        for subplan in plan.split('\n') :
+            hour = int(subplan.split(':')[0].strip(' '))
+            minute = subplan.split(':')[1].strip(' ')
+            action = subplan.split(':')[2]
+
+            plan_time = f"{hour}:{minute[:-2].strip(' ')}" if minute.endswith("am") else f"{hour + 12}:{minute[:-2]}"
+
+            self.plans[str(time.date())]['detail_plan'].append({
+                "plan" : action,
+                "time" : plan_time
+            })
 
     def __gen_summary_description__(self , time:datetime) -> str:
         ### gen [Agent’s Summary Description] ###
@@ -132,7 +214,7 @@ class Agent:
         self.update_observation([reflect] , time)
     
     
-    def __gen_reaction__(self , observation , time):
+    def __gen_reaction__(self , observation : dict , time : datetime) -> Tuple[bool , str]:
         ### from observation decide whether to react ###
         # TODO retrieval A & B's summary / add into action
 
@@ -142,60 +224,118 @@ class Agent:
         query_B = f"{observation['observation']}"
 
         memory = self.__gen_retrieval__(query_A , 10 , time) + self.__gen_retrieval__(query_B , 10 , time)
-        statements = [i['observation'] for i in memory]
+
+        ### !!! TODO memory summary !!! ###
+        summary = "summary"
+
         
         prompt = (
             f"{agent_summary}\n"
             f"It is {str(time)}\n"
             f"{self.name}'s status: {self.current_action}\n"
-            f"Observation: {observation}\n"
+            f"Observation: {observation['observation']}\n"
             # need retrieval A & B's summary
             f"Summary of relevant context from {self.name}'s memory: \n"
-            
+            f"{summary}"
         )
 
-        if self.current_conversation == None:
-            prompt += (f"Should {self.name} react to the observation, and if so, what would be an appropriate reaction?\n")
-            ### !!! TODO LLM output !!! ###
-            react = "John is asking Eddy about his music composition project"
-            dialogue_prompt = '\n'.join(prompt.split("\n")[:-1])
-            dialogue_prompt += ( 
+        react_prompt = prompt + f"Should {self.name} react to the observation, and if so, what would be an appropriate reaction?\n"
+            
+        ### !!! TODO LLM output !!! ###
+        react = "John is asking Eddy about his music composition project"
+        ### !!! TODO : Detect LLM output is react or not !!! ###
+        is_react = True
+        if not is_react:
+            return False
+        
+        if observation['entity_type'] == 0:
+            self.current_action = react
+        else:
+            dialogue_prompt = prompt + ( 
                 f"{react}"
                 f"What would he say to {observation['observed_entity']}?"
             )
-        else:
-            prompt += (
-                f"{self.current_conversation}"
-                f"How would {self.name} respond to {observation['observed_entity']}?"
-            )
-        
+            
+            ### !!! TODO LLM output !!! ###
+            conversation = "Hello"
 
-        self.__gen_dialogue__(dialogue_prompt , time)
+            self.current_conversation = {
+                "create_time" : str(time),
+                "conversation" : f"{self.name} : {conversation}\n",
+                "target"  : observation['observed_entity'],
+                "target_action" : ""
+            }
+
+            ### !!! TODO summary of conversation !!! ###
+            summary = "convsersation summary"
+            self.current_action = f"conversing about {summary}"
+
+            return True
+
+        
 
     
-    def __gen_dialogue__(self , dialogue_prompt , time):
+    def __gen_dialogue__(self , time) -> bool:
         ### agent dialogue ###
+        agent_summary =  self.__gen_summary_description__(time)
         
+        query_A = f"What is {self.name}'s relationship with the {self.current_conversation['target']} ?"
+        query_B = f"{self.current_conversation['target_action']}"
+
+        memory = self.__gen_retrieval__(query_A , 10 , time) + self.__gen_retrieval__(query_B , 10 , time)
+    
+        ### !!! TODO memory summary !!! ###
+        summary = "summary"
+        
+        prompt = (
+            f"{agent_summary}\n"
+            f"It is {str(time)}\n"
+            f"{self.name}'s status: {self.current_action}\n"
+            f"Observation: {self.current_conversation['target_action']}\n"
+            # need retrieval A & B's summary
+            f"Summary of relevant context from {self.name}'s memory: \n"
+            f"{summary}"
+            f"How would {self.name} respond to {self.current_conversation['target']}?"
+        )
 
         ### !!! TODO LLM output !!! ###
         conversation = "Hello"
+        ### !!! TODO Detect LLM output is continue or not !!! ###
+        continue_conversation = True
 
-        # agent not have conversation now 
-        if self.current_conversation == None:
-            self.current_conversation = {
-                "create_time" : str(time),
-                "conversation" : f"{self.name} : {conversation}\n"
-            }
-        # agent have conversation now 
-        elif conversation != None:
+        if continue_conversation:
             self.current_conversation["conversation"] += f"{self.name} : {conversation}\n"
-        # agent decide end the conversation
+            ### !!! TODO summary of conversation !!! ###
+            summary = "convsersation summary"
+            self.current_action = f"conversing about {summary}"
+            return True
         else:
             self.conversation_history.append(self.current_conversation)
             self.current_conversation = None
+            return False
 
+    def __agent_move__(self , places):
+        ### agent move to new node ###
+        ### TODO recursive until leaf node ###
+        places = ' , '.join(places)
+        knows = ' , '.join(self.knows_tree.__iter_around_env__(self.knows_tree.root))        
+
+        prompt = (
+            f"{self.name} is currently in {self.location}) that has {places}"
+            f"{self.name} knows of the following areas: {knows}"
+            "* Prefer to stay in the current area if the activity can be done there."
+            f"{self.name} is planning {self.current_action}"
+            f"Which area should {self.name} go to?"
+        )
+
+        ### !!! TODO LLM output !!! ###
+        area = "NUK Town:7-11:counter:checkout counter 1"
+        self.location = area
+        
+        
 
     def __str__(self) -> str:
+        ### TODO show more info ###
         ret = (
             f"-- {self.name} / Age : {self.age} --\n"
             "Personality and Lifestyle : \n"
--- utils/generative_agents.py
+++ utils/generative_agents.py
@@ -8,20 +8,43 @@ class generative_agents():
     
     def __init__(self , env_json : Union[str , Path], agents_path : Union[str , Path], init_time : datetime = datetime.datetime(2023,7,9,00,00,00,00) , tick :int= 10):
         self.env = env_tree(env_json)
-        self.agents : list[Agent] = []
+        self.agents : dict[str , Agent]  = {}
         self.time = init_time
         self.tick = tick
         for agent in Path(agents_path).glob("*.json"):
-            self.agents.append(Agent(agent))
+            new_agent = Agent(agent)
+            self.agents[new_agent.name] = new_agent
+            self.env.add_agent(new_agent.location , new_agent.name , "idle")
 
     def next_tick(self):
+        ### system time add tick and all agents update tick ###
         self.time += datetime.timedelta(seconds=self.tick)
-
-        for agent in self.agents:
+        
+        # all agent get prev tick observation & places
+        for agent in self.agents.values():
             observations , places = self.env.observation(agent.location)
-            agent.update_observation(observations , self.time)
-            agent.update_knows_places(places)
+            agent.next_tick(observations, places , self.time , 0.5)
 
+        # clear prev tick's agents location
+        self.env.clear_all_agent()
+
+        
+        for agent in self.agents.values():
+            # update this tick agents location
+            self.env.add_agent(agent.location , agent.name , agent.current_action)
+
+            # if agent have conversation , 
+            # update the conversation to it's target
+            # and swap conversation target
+            if agent.current_conversation != None:
+                self.agents[agent.current_conversation['target']].current_conversation = agent.current_conversation
+                agent.current_conversation['target'] = agent.name
+                
+
+
+    def new_day(self , time):
+        for agent in self.agents.values():
+            agent.start_of_day(time)
     
     
 
